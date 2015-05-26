aeda619e222e686f52b4a60ca8deefd45292910f
pigeonflight <david@alteroo.com>
2015-05-26 02:23:06 +0000

    setup.py now looks for README.rst

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '0.0.1'
 setup(name='pythonjamaica.profile',
       version=version,
       description="PythonJamaica Profile for Exam Platform",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
